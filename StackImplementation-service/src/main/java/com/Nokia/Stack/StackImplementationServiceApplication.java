a38b2db46663c1c084ad86213ca36e3e6189cf9c
Vijay RS <rsvijay9988@gmail.com>
2020-09-09 18:38:03 +0530

    initial commit

---
+++ StackImplementation-service/src/main/java/com/Nokia/Stack/StackImplementationServiceApplication.java
@@ -0,0 +1,17 @@
+package com.Nokia.Stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.Nokia.Stack")
+@EnableDiscoveryClient
+public class StackImplementationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackImplementationServiceApplication.class, args);
+	}
+
+}
